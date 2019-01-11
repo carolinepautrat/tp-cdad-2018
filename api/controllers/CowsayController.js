b47a39fc5cc3b34e47f9f56f79c52d4bfe1fc2b3
Caroline Pautrat <caroline.pautrat@outlook.fr>
2019-01-11 16:03:27 +0100

    Téléversement d'un fichier

--- api/controllers/CowsayController.js
+++ api/controllers/CowsayController.js
@@ -14,13 +14,22 @@ module.exports = {
   say: async function (req, res) {
     let count = await Sentences.count();
     console.debug('Got '+count+' sentences in database');
-    let s = await Sentences.find().limit(1).
+    var s = await Sentences.find().limit(1).
       skip(Math.floor(Math.random() * Math.floor(count)));
     let sentence = "Random Message";
     if(s.length > 0) {
       sentence = s[0].sentence;
     }
-    return res.view('cowsay', { cow: cowsay.say({
+
+    if (s[0].picture) {
+      const options = {
+        key: 'AKIAJOCSBD4KTGNIE2YQ' ,
+        secret: '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',
+        bucket : 'lp-cdad-2018'
+      };
+    }
+
+    return res.view('cowsay', { picture: s[0].picture, cow: cowsay.say({
       f: process.env.COW || 'stegosaurus',
       text : sentence,
       e : 'oO',
@@ -33,8 +42,38 @@ module.exports = {
   },
 
   create: async function(req, res) {
-    await Sentences.create({ sentence: req.param('sentence') });
-    return res.redirect('/say');
+    const options = {
+      adapter: require('skipper-better-s3'),
+      key: 'AKIAJOCSBD4KTGNIE2YQ' ,
+      secret: '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',
+      bucket : 'lp-cdad-2018',
+      region: 'eu-west-3',
+      s3params: {
+        ACL: 'public-read'
+      }
+    }
+
+    req.file('file').upload(options, async (err, uploadedFiles) => {
+      if (err) return res.serverError(err);
+
+      await Sentences.create({
+        sentence: req.param('sentence'), picture: uploadedFiles[0].extra.Location 
+      })
+      return res.redirect('/say');
+    })
+    /*await req.file('file').upload({
+      dirname: require('path').resolve(sails.config.appPath, 'assets/images')
+    }, async function (err, uploadedFiles) {
+      if (err) return res.serverError(err);
+
+      var tmp_split = uploadedFiles[0].fd.split('/');
+      var saved_filename = tmp_split[tmp_split.length-1];
+
+      await Sentences.create({
+        sentence: req.param('sentence'), picture: saved_filename 
+      })
+      return res.redirect('/say');
+    })*/
   },
 };
 
